af11e6ce2b4dcc20990e17a04ac9d02716e4a6e7
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2024-01-30 10:33:19 -0500

    Create controllers and implement exception filter

---
+++ Tarker.Booking.Api/Controllers/Usercontroller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tarker.Booking.Application.Exceptions;
+
+namespace Tarker.Booking.Api.Controllers
+{
+  [Route("api/v1/user")]
+  [ApiController]
+  [TypeFilter(typeof(ExceptionManager))]
+  public class Usercontroller : ControllerBase
+  {
+    public Usercontroller()
+    {
+    }
+  }
+}
--- Tarker.Booking.Api/Program.cs
+++ Tarker.Booking.Api/Program.cs
@@ -14,6 +14,9 @@ builder
   .AddExternal(builder.Configuration)
   .AddPersistence(builder.Configuration);
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
+app.MapControllers();
 app.Run();
